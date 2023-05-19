3a3edeea151aaa7c9d106d7d39bd63b73f09c662
Harsh Bharadwaj <harsh.bharadwaj.cv@gmail.com>
2023-05-19 10:37:00 +0530

    vercel changes2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path=require("path");
 
 
 const app = express();
-app.set('views', [path.join(__dirname, 'views')]);
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
