bb23c47e6f3db45559d9ee1edc2b127c404f40a0
Harsh Bharadwaj <harsh.bharadwaj.cv@gmail.com>
2023-07-06 12:56:38 +0530

    Re Commit

--- Assignment/backend/db/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://admin-harsh:"+process.env.MONGO_KEY+"@cluster0.pqd8uax.mongodb.net/assignDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-console.log("Connected to database");
